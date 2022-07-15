a7da3b91a2cc965a17168144dbbd92b94531e90a
Bissixp <fernandosbissi18@hotmail.com>
2022-07-15 16:36:33 -0300

    Pequena correção para funcionar o debug

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,7 +3,7 @@ const auth = require('../services/authService');
 
 const loginController = {
   /** @type {import('express').RequestHandler} */
-  async getToken(req, res) {
+  async login(req, res) {
     const user = await loginService.validadeBody(req.body);
     const checkUser = await loginService.checkUser(user);
     const token = await auth.makeToken(checkUser);
